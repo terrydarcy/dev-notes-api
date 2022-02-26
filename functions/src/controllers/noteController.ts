35fcf14928ccffb1b6a853e3bfe2a55fcf1e7c1e
Terry D'Arcy <terrykdarcy@gmail.com>
2022-02-26 20:16:38 +0000

    sorting by timestamp on notes

--- functions/src/controllers/noteController.ts
+++ functions/src/controllers/noteController.ts
@@ -1,17 +1,18 @@
 const db = require("../db");
 interface Note {
   id: number;
+  note_title: string;
   user_id: string;
   note_text: string;
   timestamp: Date;
 }
 
 const setNote = async (req: any, res: any, next: any) => {
-  const { user_id, note_text, timestamp }: Note = req.body;
+  const { note_title, user_id, note_text, timestamp }: Note = req.body;
 
-  if (user_id && note_text && timestamp) {
-    const queryString ="INSERT INTO notes(user_id, note_text, timestamp) VALUES($1, $2, $3) RETURNING *";
-    const values = [user_id, note_text, timestamp];
+  if (user_id && note_text && timestamp && note_title) {
+    const queryString ="INSERT INTO notes(user_id, note_text, timestamp, note_title) VALUES($1, $2, $3, $4) RETURNING *";
+    const values = [user_id, note_text, timestamp, note_title];
 
     await db.query(queryString, values)
       .then((data: any) => {
@@ -27,13 +28,12 @@ const getUserNotes = async (req: any, res: any, next: any) => {
   const { id } = req.params;
 
   if (id) {
-    const queryString ="SELECT * FROM notes WHERE user_id = $1";
+    const queryString ="SELECT * FROM notes WHERE user_id = $1 ORDER BY timestamp DESC";
     const values = [id];
 
     await db.query(queryString, values)
       .then((data: any) => {
-        console.log(data.rows);
-        res.send(data.rows);
+         res.send(data.rows);
       })
       .catch((e: any) => {
         next(e.stack);
